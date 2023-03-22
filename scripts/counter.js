81b87982b9aff8e99fc0b38919c589f71b62645f
awkokjun-nm2207 <125463303+awkokjun-nm2207@users.noreply.github.com>
2023-03-22 20:21:31 +0800

    Update counter.js

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,18 +1,40 @@
-body {
-  font-family: sans-serif;
-  margin: 0;
-  padding: 0;
-}
+const dataUrl = "https://raw.githubusercontent.com/ChatGPT/data/main/livestream_ecommerce_data.json";
 
-h1 {
-  text-align: center;
-}
+d3.json(dataUrl).then(data => {
+  const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+  const width = 800 - margin.left - margin.right;
+  const height = 600 - margin.top - margin.bottom;
 
-#chart {https://github.com/awkokjun-nm2207/awkokjun-nm2207.github.io/blob/main/scripts/counter.js
-  margin: 0 auto;
-  max-width: 800px;
-  height: 600px;
-  background-color: #f7f7f7;
-}
+  const svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  const x = d3.scaleBand()
+    .domain(data.map(d => d.product))
+    .range([0, width])
+    .padding(0.1);
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.sales)])
+    .range([height, 0]);
+
+  svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x));
+
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  svg.selectAll(".bar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", d => x(d.product))
+    .attr("y", d => y(d.sales))
+    .attr("width", x
 
 
