7db2491db7fba9f5063de8fa5c716b34500f669b
awkokjun-nm2207 <125463303+awkokjun-nm2207@users.noreply.github.com>
2023-03-22 20:18:28 +0800

    Update counter.js

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,17 +1,18 @@
-let counter = 0;
+body {
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
+}
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("button").onclick = count;
-});
+h1 {
+  text-align: center;
+}
 
-function count() {
-  counter++;
-
-  if (counter % 10 === 0) {
-    alert("Mouse would like to thank you for the food 🥙");
-  }
-
-  document.querySelector(".counter").innerHTML = counter;
+#chart {https://github.com/awkokjun-nm2207/awkokjun-nm2207.github.io/blob/main/scripts/counter.js
+  margin: 0 auto;
+  max-width: 800px;
+  height: 600px;
+  background-color: #f7f7f7;
 }
 
 
